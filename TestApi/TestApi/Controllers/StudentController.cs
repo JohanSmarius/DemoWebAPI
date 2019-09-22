7e27daa747568dd9b20f07ac7f795ef7036d41b4
Johan Smarius <jsmarius@hotmail.com>
2019-09-22 22:27:11 +0200

    Code cleanup

--- TestApi/TestApi/Controllers/StudentController.cs
+++ TestApi/TestApi/Controllers/StudentController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TestApi.Models;
 
@@ -11,7 +8,6 @@ namespace TestApi.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
-        // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<Student>> Get()
         {
@@ -22,16 +18,14 @@ namespace TestApi.Controllers
             };
         }
 
-        // GET api/values/5
         [FormatFilter]
         [Produces("application/json", "application/xml")]
         [HttpGet("{id}/{format?}")]
         public ActionResult<Student> Get(int id)
         {
-            return new Student() {Name = "Steve Ballmer", Number = 1};
+            return new Student() {Name = $"Student{id}", Number = 1};
         }
 
-        // POST api/values
         [HttpPost]
         [Consumes("application/xml")]
         public IActionResult PostXml([FromBody] Student student)
@@ -44,19 +38,6 @@ namespace TestApi.Controllers
         public IActionResult PostJson([FromBody] Student student)
         {
             return Ok(student);
-        }
-
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
